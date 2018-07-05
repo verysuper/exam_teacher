50c56593fc58214617448d420eea6a301b54686d
verysuper <verysuper@live.com>
2018-07-05 14:02:16 +0800

    3-1

--- q1/admin_1_3.php
+++ q1/admin_1_3.php
@@ -1,3 +1,11 @@
+<?php
+  if(!empty($_POST["myalt"])){
+    $tt = date("YmdHis");
+    $sql="insert into a_1_3_title_pic value(Null,'".$tt.".jpg','".$_POST["myalt"]."','0')";
+    mysqli_query($link,$sql);
+    copy($_FILES["mypic"]["tmp_name"],"nfgkjewqrhto3ty23984rh9fh32f/".$tt.".jpg");
+  }
+?>
 <div style="width:99%; height:87%; margin:auto; overflow:auto; border:#666 1px solid;">
   <p class="t cent botli">網站標題管理</p>
   <form method="post" target="back" action="?do=tii">
@@ -15,7 +23,7 @@
     <table style="margin-top:40px; width:70%;">
       <tbody>
         <tr>
-          <td width="200px"><input type="button" onclick="op(&#39;#cover&#39;,&#39;#cvr&#39;,&#39;view.php?do=title&#39;)" value="新增網站標題圖片"></td>
+          <td width="200px"><input type="button" onclick="op('#cover','#cvr','admin_1_3_add_pic.php')" value="新增網站標題圖片"></td>
           <td class="cent"><input type="submit" value="修改確定"><input type="reset" value="重置"></td>
         </tr>
       </tbody>
