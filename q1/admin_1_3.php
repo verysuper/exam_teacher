c3784b42ec783529870ceb1d8c07174582dc812b
verysuper <verysuper@live.com>
2018-07-05 16:03:50 +0800

    3-1

--- q1/admin_1_3.php
+++ q1/admin_1_3.php
@@ -3,8 +3,24 @@
     $tt = date("YmdHis");
     $sql="insert into a_1_3_title_pic value(Null,'".$tt.".jpg','".$_POST["myalt"]."','0')";
     mysqli_query($link,$sql);
-    copy($_FILES["mypic"]["tmp_name"],"nfgkjewqrhto3ty23984rh9fh32f/".$tt.".jpg");
+    copy($_FILES["mypic"]["tmp_name"],"pic/".$tt.".jpg");
     ?><script>document.location.href="admin.php"</script><?php
+  }
+  if(isset($_POST["my_alt"][0])){
+    for($i=0;$i<count($_POST["my_no"]);$i++){
+      $sql = "update a_1_3_title_pic set a_1_3_t_p_alt = '".$_POST["my_alt"][$i]."', a_1_3_t_p_look = 0 where a_1_3_t_p_seq = '".$_POST["my_no"][$i]."'";
+      mysqli_query($link,$sql);
+      
+      if(!empty($_POST["mydelete"][$i])){
+        $sql = "select * from a_1_3_title_pic where a_1_3_t_p_seq = '".$_POST["my_no"][$i]."'";
+        $or = mysqli_query($link,$sql);
+        $oo = mysqli_fetch_assoc($or);
+        echo $oo["a_1_3_t_p_title"];
+      }
+      
+    }
+    $sql = "update a_1_3_title_pic set a_1_3_t_p_look = 1 where a_1_3_t_p_seq = '".$_POST["myupdate"]."'";
+    mysqli_query($link,$sql);
   }
     $sql="select * from a_1_3_title_pic";
     $or =mysqli_query($link,$sql);
@@ -23,7 +39,7 @@
         </tr>
 <?php do{?>
         <tr>
-          <td width="45%"><img src="nfgkjewqrhto3ty23984rh9fh32f/<?=$oo["a_1_3_t_p_title"]?>" width="300" height="30"></td>
+          <td width="45%"><img src="pic/<?=$oo["a_1_3_t_p_title"]?>" width="300" height="30"></td>
           <td width="23%">
             <input name="my_alt[]" value="<?=$oo["a_1_3_t_p_alt"]?>">
             <input type ="hidden" name="my_no[]" value="<?=$oo["a_1_3_t_p_seq"]?>">
@@ -31,7 +47,7 @@
           <td width="7%">
             <input name="myupdate" type="radio" value="<?=$oo["a_1_3_t_p_seq"]?>" <?php if($oo["a_1_3_t_p_look"]==1){echo "checked"; }?>>
           </td>
-          <td width="7%"><input type="checkbox" name="mydelete[]" value="1"></td>
+          <td width="7%"><input type="checkbox" name="mydelete[]" value="<?=$oo["a_1_3_t_p_seq"]?>"></td>
           <td></td>
         </tr>
 <?php }while($oo = mysqli_fetch_assoc($or));?>
